6a16c7cd01d411387f3cf021190aade9fc9e810f
Tsuixy <190241347@qq.com>
2016-10-10 11:03:11 +0800

    初始化

---
+++ rdpRemote/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace rdpRemote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(924, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "刷新主机状态";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click_1);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "打开配置文件";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(924, 390);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "重新加载数据";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 415);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "外挂";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+    }
+}
+
---
+++ rdpRemote/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace rdpRemote
+{
+
+    public partial class Form1 : Form
+    {
+        public class ConnectionInfo
+        {
+            public string 地址 { get; set; }
+            public string 用户名 { get; set; }
+            public string 密码 { get; set; }
+            public string 文件名 { get; set; }
+            public string 备注 { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string configPath = System.Windows.Forms.Application.StartupPath + @"\Resources\ServersConfig.xml";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitDataGridView();
+           
+
+        }
+
+        private void InitDataGridView()
+        {
+            List<ConnectionInfo> result = new List<ConnectionInfo>();
+
+            XmlOp xml = new XmlOp(configPath);
+            var connectInfo = new ConnectionInfo();
+            var root = xml.GetXmlRoot();
+            var nodeList = root.GetEnumerator();
+            while (nodeList.MoveNext())
+            {
+                XmlAttributeCollection temp = ((XmlNode)nodeList.Current).Attributes;
+                var subNode = temp.GetEnumerator();
+                var conNode = new ConnectionInfo();
+                while (subNode.MoveNext())
+                {
+                    XmlAttribute attribute = (XmlAttribute)subNode.Current;
+                    if (attribute.Name == "ip")
+                    {
+                        conNode.地址 = attribute.Value;
+                    }
+                }
+                conNode.用户名 = xml.GetXmlNodeValue(@"/servers/server[@ip='" + conNode.地址 + "']/UserName");
+                conNode.文件名 = xml.GetXmlNodeValue(@"/servers/server[@ip='" + conNode.地址 + "']/FileName");
+                conNode.密码 = xml.GetXmlNodeValue(@"/servers/server[@ip='" + conNode.地址 + "']/PassWord");
+                conNode.备注 = xml.GetXmlNodeValue(@"/servers/server[@ip='" + conNode.地址 + "']/Remark");
+                result.Add(conNode);
+            }
+            dataGridView1.DataSource = result;
+            dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            DataGridViewButtonColumn connectBtn = new DataGridViewButtonColumn();
+            connectBtn.HeaderText = "远程登录";
+            connectBtn.Text = "远程登录";
+            connectBtn.UseColumnTextForButtonValue = true;
+            connectBtn.AutoSizeMode =
+                DataGridViewAutoSizeColumnMode.AllCells;
+            connectBtn.FlatStyle = FlatStyle.Standard;
+            connectBtn.CellTemplate.Style.BackColor = Color.Honeydew;
+            dataGridView1.Columns.Add(connectBtn);
+
+            DataGridViewButtonColumn closeBtn = new DataGridViewButtonColumn();
+            closeBtn.HeaderText = "远程关机";
+            closeBtn.Text = "远程关机";
+            closeBtn.UseColumnTextForButtonValue = true;
+            closeBtn.AutoSizeMode =
+                DataGridViewAutoSizeColumnMode.AllCells;
+            closeBtn.FlatStyle = FlatStyle.Standard;
+            closeBtn.CellTemplate.Style.BackColor = Color.Honeydew;
+            dataGridView1.Columns.Add(closeBtn);
+
+
+            DataGridViewButtonColumn pingBtn = new DataGridViewButtonColumn();
+            pingBtn.HeaderText = "Ping";
+            pingBtn.Text = "Ping";
+            pingBtn.UseColumnTextForButtonValue = true;
+            pingBtn.AutoSizeMode =
+                DataGridViewAutoSizeColumnMode.AllCells;
+            pingBtn.FlatStyle = FlatStyle.Standard;
+            pingBtn.CellTemplate.Style.BackColor = Color.Honeydew;
+            dataGridView1.Columns.Add(pingBtn);
+
+            DataGridViewImageColumn imageCell = new DataGridViewImageColumn();
+            imageCell.HeaderText = "状态";
+            imageCell.Image = Image.FromFile(Application.StartupPath + @"\Resources\red.jpg");
+            imageCell.AutoSizeMode =
+                DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns.Add(imageCell);
+        }
+        private void DoPing(int rowIndex)
+        {
+            //tim.Enabled = false;
+            Application.DoEvents();
+            var con = new ConnectionInfo();
+            con.地址 = this.dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+            con.用户名 = this.dataGridView1.Rows[rowIndex].Cells[1].Value.ToString();
+            con.密码 = this.dataGridView1.Rows[rowIndex].Cells[2].Value.ToString();
+            con.文件名 = this.dataGridView1.Rows[rowIndex].Cells[3].Value.ToString();
+            con.备注 = this.dataGridView1.Rows[rowIndex].Cells[4].Value.ToString();
+            if (Ping(con.地址))
+            {
+                dataGridView1.Rows[rowIndex].Cells[8].Value = Image.FromFile(Application.StartupPath + @"\Resources\green.jpg");
+            }
+            else
+            {
+                dataGridView1.Rows[rowIndex].Cells[8].Value = Image.FromFile(Application.StartupPath + @"\Resources\red.jpg");
+            }
+            // tim.Enabled = true;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 5) //远程登录
+            {
+                var con = new ConnectionInfo();
+                con.地址 = this.dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                con.用户名 = this.dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                con.密码 = this.dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                con.文件名 = this.dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                con.备注 = this.dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                Connection(con);
+            }
+            if (e.ColumnIndex == 6) //关机
+            {
+                var con = new ConnectionInfo();
+                con.地址 = this.dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                con.用户名 = this.dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                con.密码 = this.dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                con.文件名 = this.dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                con.备注 = this.dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                DialogResult dr = MessageBox.Show("确定要关闭吗?", "关闭系统", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    RemoteClose(con);
+                }
+            }
+            if (e.ColumnIndex == 7) //Ping
+            {
+                var con = new ConnectionInfo();
+                con.地址 = this.dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                con.用户名 = this.dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                con.密码 = this.dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                con.文件名 = this.dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                con.备注 = this.dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                if (Ping(con.地址))
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[8].Value = Image.FromFile(Application.StartupPath + @"\Resources\green.jpg");
+                }
+                else
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[8].Value = Image.FromFile(Application.StartupPath + @"\Resources\red.jpg");
+                }
+            }
+        }
+
+        #region FUNC
+        private bool Ping(string address)
+        {
+            Ping p1 = new Ping();
+
+            PingReply reply = p1.Send(address);
+
+            StringBuilder sbuilder;
+            if (reply.Status == IPStatus.Success)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void RemoteClose(ConnectionInfo con)
+        {
+            List<string> cmdLst = new List<string>();
+            var cmd0 = "net use \\\\" + con.地址 + "\\ipc$ \"" + con.密码 + "\" /user:\"" + con.用户名 + "\"";
+            var cmd1 = "shutdown -s -m \\\\" + con.地址 + " -t 10";
+            cmdLst.Add(cmd0);
+            cmdLst.Add(cmd1);
+            ExeCommand(cmdLst.ToArray());
+        }
+        private void RemoteLogin(String cmd)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(cmd);
+            p.StandardInput.WriteLine("exit");
+        }
+        private void Connection(ConnectionInfo con)
+        {
+            string address = con.地址;
+            string username = con.用户名;
+            string password = con.密码;
+            string filename = con.文件名;
+            var TemplateStr = rdpRemote.Properties.Resources.TemplateRDP;//获取RDP模板字符串
+            //用DataProtection加密密码,并转化成二进制字符串
+            var pwstr = BitConverter.ToString(DataProtection.ProtectData(Encoding.Unicode.GetBytes(password), ""));
+            pwstr = pwstr.Replace("-", "");
+            //替换模板里面的关键字符串,生成当前的drp字符串
+            var NewStr = TemplateStr.Replace("{#address}", address).Replace("{#username}", username).Replace("{#password}", pwstr);
+            //将drp保存到文件，并放在程序目录下，等待使用
+            StreamWriter sw = new StreamWriter(filename);
+            sw.Write(NewStr);
+            sw.Close();
+            //利用CMD命令调用MSTSC
+            RemoteLogin("mstsc " + filename);
+        }
+        public static string ExeCommand(string[] commandTexts)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            string strOutput = null;
+            try
+            {
+                p.Start();
+                foreach (string item in commandTexts)
+                {
+                    p.StandardInput.WriteLine(item);
+                }
+                p.StandardInput.WriteLine("exit");
+                strOutput = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception e)
+            {
+                strOutput = e.Message;
+            }
+            return strOutput;
+        }
+        #endregion
+
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                DoPing(i);
+
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe", configPath);
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            InitDataGridView();
+        }
+    }
+}
